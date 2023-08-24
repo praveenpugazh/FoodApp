8ef5c0359aab0d5b7a99aa53ba5d190ae76664f6
praveenpugazh <praveenpugazh14@gmail.com>
2023-08-24 22:32:07 +0530

    added new changes

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,9 +1,11 @@
+/* eslint-disable no-unsafe-optional-chaining */
 import { IMG_URL } from '../utils/contants'
 import { Link } from 'react-router-dom'
 // eslint-disable-next-line react/prop-types
 const RestaurantCard = ({ resData }) => {
   // eslint-disable-next-line react/prop-types
   const { name, avgRating, locality, cuisines, cloudinaryImageId, id } =
+    // eslint-disable-next-line react/prop-types
     resData?.info
 
   // const IMG_URL =
@@ -11,7 +13,7 @@ const RestaurantCard = ({ resData }) => {
 
   return (
     <Link to={`res/${id}`}>
-      <div className='mr-4 mb-8 h-80 w-60 shadow-md hover:shadow-lg cursor-pointer'>
+      <div className='mr-4 mb-8 h-96 w-60 shadow-md hover:shadow-lg cursor-pointer rounded-md'>
         <div className='w-60 h-40'>
           <img
             className='w-60 h-40'
@@ -24,7 +26,11 @@ const RestaurantCard = ({ resData }) => {
         <div className='cardContent p-4'>
           <h1 className='font-bold'>{name}</h1>
           <p className=''>{avgRating} ⭐</p>
-          <p className='text-gray-500'>{cuisines.join(', ')}</p>
+          <p className='text-gray-500'>
+            {cuisines.length < 6
+              ? cuisines.join(', ')
+              : cuisines.join(', ').substr(0, 60)}{' '}
+          </p>
           <p className='text-gray-500'>Location: {locality}</p>
         </div>
       </div>
