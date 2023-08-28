1f9d75220c9ac43339ce2cc44d70cfa5dad223e6
praveenpugazh <praveenpugazh14@gmail.com>
2023-08-28 11:43:34 +0530

    name was left

--- src/components/SingleCardMenu.jsx
+++ src/components/SingleCardMenu.jsx
@@ -2,7 +2,8 @@ import { IMG_URL } from '../utils/contants'
 
 // eslint-disable-next-line react/prop-types
 const SingleCardMenu = ({ value, show }) => {
-  const { imageId, description, finalPrice, price, isVeg } = value.card.info
+  const { imageId, description, finalPrice, price, isVeg, name } =
+    value.card.info
 
   return (
     <>
@@ -10,7 +11,7 @@ const SingleCardMenu = ({ value, show }) => {
         <div className='mr-4 mb-4 h-20 shadow-md hover:shadow-xl flex'>
           <img src={IMG_URL + imageId} alt='food image' width='100px' />
           <div className='ml-4'>
-            <p className='font-bold'>{name}</p>
+            <p className=''>{name}</p>
             <p>₹ {finalPrice ? finalPrice / 100 : price / 100}</p>
             <p className='text-slate-400'>{description}</p>
             <p className='text-slate-400'>{isVeg ? '🟢' : '🔴'}</p>
